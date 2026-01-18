8e533e6f49aa2fe6a1c985501a967eaab8db681f
Harry-Huynh <hphuynh05@gmail.com>
2026-01-18 17:52:12 -0500

    feat: Add saving goal's routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const { passport } = require('../lib/passport');
 const transactionService = require('../lib/services/transaction-service');
+const savingGoalService = require('../lib/services/saving-goal-service');
 
 const router = express.Router();
 
+// Transaction Routes
 router.post('/transaction', passport.authenticate('jwt', { session: false }), async (req, res) => {
   try {
     const userId = req.user.userId;
     const transMsg = await transactionService.addTransaction(userId, req.body);
     return res.status(201).json({ message: transMsg });
   } catch (e) {
-    return res.status(422).json({ message: String(e) });
+    return res.status(422).json({ message: e.message });
   }
 });
 
@@ -24,7 +26,7 @@ router.get(
       const transaction = await transactionService.getTransaction(userId, id);
       return res.status(200).json({ transaction });
     } catch (e) {
-      return res.status(404).json({ message: String(e) });
+      return res.status(404).json({ message: e.message });
     }
   }
 );
@@ -34,11 +36,11 @@ router.get('/transactions', passport.authenticate('jwt', { session: false }), as
     const userId = req.user.userId;
     const transactions = await transactionService.getAllTransactions(userId);
     return res.status(200).json({
-      count: transactions.length, // may put it here or can check at front end
+      count: transactions.length,
       transactions,
     });
   } catch (e) {
-    return res.status(500).json({ message: String(e) });
+    return res.status(500).json({ message: e.message });
   }
 });
 
@@ -52,7 +54,80 @@ router.delete(
       const deleteMsg = await transactionService.deleteTransaction(userId, transactionId);
       return res.status(200).json({ message: deleteMsg });
     } catch (e) {
-      return res.status(422).json({ message: String(e) });
+      return res.status(422).json({ message: e.message });
+    }
+  }
+);
+
+// Saving Goal Routes
+router.post('/saving-goal', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const savingGoalMsg = await savingGoalService.addSavingGoal(userId, req.body);
+    return res.status(201).json({ message: savingGoalMsg });
+  } catch (e) {
+    return res.status(422).json({ message: e.message });
+  }
+});
+
+router.get('/saving-goals', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const savingGoals = await savingGoalService.getAllSavingGoals(userId);
+    return res.status(200).json({
+      count: savingGoals.length,
+      savingGoals,
+    });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+});
+
+router.get(
+  '/saving-goal/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    try {
+      const userId = req.user.userId;
+      const savingGoalId = req.params.id;
+      const savingGoal = await savingGoalService.getSavingGoal(userId, savingGoalId);
+      return res.status(200).json({ savingGoal });
+    } catch (e) {
+      return res.status(404).json({ message: e.message });
+    }
+  }
+);
+
+router.put(
+  '/saving-goal/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    try {
+      const userId = req.user.userId;
+      const savingGoalId = req.params.id;
+      const savingGoalMsg = await savingGoalService.updateSavingGoal(
+        userId,
+        savingGoalId,
+        req.body
+      );
+      return res.status(200).json({ message: savingGoalMsg });
+    } catch (e) {
+      return res.status(422).json({ message: e.message });
+    }
+  }
+);
+
+router.delete(
+  '/saving-goal/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    try {
+      const userId = req.user.userId;
+      const savingGoalId = req.params.id;
+      const savingGoalMsg = await savingGoalService.deleteSavingGoal(userId, savingGoalId);
+      return res.status(200).json({ message: savingGoalMsg });
+    } catch (e) {
+      return res.status(422).json({ message: e.message });
     }
   }
 );
